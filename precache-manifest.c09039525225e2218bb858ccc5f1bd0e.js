0df497d82b6bfa099157b2ca4414281b9b43a1b8
Julio Espadas <julio.espadas@gmail.com>
2019-08-16 16:21:21 +0200

    Updates

---
+++ precache-manifest.c09039525225e2218bb858ccc5f1bd0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae13f04ecab4a32915cf",
+    "url": "/static/js/main.8a24b757.chunk.js"
+  },
+  {
+    "revision": "1be96da4163ad331184e",
+    "url": "/static/js/2.92eb3a45.chunk.js"
+  },
+  {
+    "revision": "ae13f04ecab4a32915cf",
+    "url": "/static/css/main.c306bcf0.chunk.css"
+  },
+  {
+    "revision": "d5ede35ba8da8c4317668e2526405bda",
+    "url": "/index.html"
+  }
+];
