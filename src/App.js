a6ac9711d4277bc200a7b1cb008436e9d70777bd
Julio Espadas <contact@julioespadas.com>
2024-05-11 12:18:37 +0200

    chore: updating

--- src/App.js
+++ src/App.js
@@ -1,147 +1,151 @@
 import React, { Component } from 'react';
-import Typist from 'react-typist';
+import { ReactTyped } from 'react-typed';
 import './App.css';
 import Configs from './configurations.json';
 import { FaHeart, FaBeer } from 'react-icons/fa';
-
+import Me from './me.jpg';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      darkBackgroundModes: [
-        'day'
-      ],
-      lightBackgroundModes: [
-        'night'
-      ],
-      backgroundType: Configs.backgroundType || 'plain',
-      appClass: Configs.plainBackgroundMode || 'daylight',
-      devIntro: Configs.devIntro,
-      devDesc: Configs.devDesc,
-      devDesc2: Configs.devDesc2,
-      backgroundMode: 'default',
-      backgroundIndex: 0,
-      bgStyle: {},
-      icons: Configs.icons || []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			darkBackgroundModes: ['day'],
+			lightBackgroundModes: ['night'],
+			backgroundType: Configs.backgroundType || 'plain',
+			appClass: Configs.plainBackgroundMode || 'daylight',
+			devIntro: Configs.devIntro,
+			devDesc: Configs.devDesc,
+			devDesc2: Configs.devDesc2,
+			backgroundMode: 'default',
+			backgroundIndex: 0,
+			bgStyle: {},
+			icons: Configs.icons || [],
+		};
+	}
 
-  componentWillMount = () => {
-    if (this.checkIfPlainTypeEnabled()) {
-      return true;
-    } else if (this.checkIfGradientTypeEnabled()) {
-      this.setState({
-        appClass: 'gradient',
-        bgStyle: this.prepareGradientStyleSheets()
-      });
-    } else if (this.checkIfImageTypeEnabled()) {
-      this.setState({
-        appClass: 'full-bg-image',
-        bgStyle: this.prepareBackgroundImageStyle()
-      });
-    }
-  };
+	componentWillMount = () => {
+		if (this.checkIfPlainTypeEnabled()) {
+			return true;
+		} else if (this.checkIfGradientTypeEnabled()) {
+			this.setState({
+				appClass: 'gradient',
+				bgStyle: this.prepareGradientStyleSheets(),
+			});
+		} else if (this.checkIfImageTypeEnabled()) {
+			this.setState({
+				appClass: 'full-bg-image',
+				bgStyle: this.prepareBackgroundImageStyle(),
+			});
+		}
+	};
 
-  checkIfNightModeEnabled = () => {
-    return (
-      this.state.backgroundType === 'plain' &&
-      this.state.appClass === 'nightlight'
-    );
-  };
+	checkIfNightModeEnabled = () => {
+		return (
+			this.state.backgroundType === 'plain' &&
+			this.state.appClass === 'nightlight'
+		);
+	};
 
-  checkIfDayModeEnabled = () => {
-    return (
-      this.state.backgroundType === 'plain' &&
-      this.state.appClass === 'daylight'
-    );
-  };
+	checkIfDayModeEnabled = () => {
+		return (
+			this.state.backgroundType === 'plain' &&
+			this.state.appClass === 'daylight'
+		);
+	};
 
-  checkIfGradientTypeEnabled = () => {
-    return this.state.backgroundType === 'gradient';
-  };
+	checkIfGradientTypeEnabled = () => {
+		return this.state.backgroundType === 'gradient';
+	};
 
-  checkIfPlainTypeEnabled = () => {
-    return this.state.backgroundType === 'plain';
-  };
+	checkIfPlainTypeEnabled = () => {
+		return this.state.backgroundType === 'plain';
+	};
 
-  checkIfImageTypeEnabled = () => {
-    return this.state.backgroundType === 'image';
-  };
+	checkIfImageTypeEnabled = () => {
+		return this.state.backgroundType === 'image';
+	};
 
-  prepareGradientStyleSheets = () => {
-    if (Configs.gradientColors) {
-      return {
-        background: 'linear-gradient(-45deg, ' + Configs.gradientColors + ')',
-        backgroundSize: '400% 400%'
-      };
-    } else {
-      return {
-        background:
-          'linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB)',
-        backgroundSize: '400% 400%'
-      };
-    }
-  };
+	prepareGradientStyleSheets = () => {
+		if (Configs.gradientColors) {
+			return {
+				background: 'linear-gradient(-45deg, ' + Configs.gradientColors + ')',
+				backgroundSize: '400% 400%',
+			};
+		} else {
+			return {
+				background:
+					'linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB)',
+				backgroundSize: '400% 400%',
+			};
+		}
+	};
 
+	changeThemeMode = (e) => {
+		if (this.checkIfNightModeEnabled()) {
+			this.setState({
+				appClass: 'daylight',
+				backgroundIndex: 0,
+				backgroundMode: this.state.darkBackgroundModes[0],
+			});
+		} else if (this.checkIfDayModeEnabled()) {
+			this.setState({
+				appClass: 'nightlight',
+				backgroundIndex: 0,
+				backgroundMode: this.state.lightBackgroundModes[0],
+			});
+		}
+	};
 
-  changeThemeMode = e => {
-    if (this.checkIfNightModeEnabled()) {
-      this.setState({
-        appClass: 'daylight',
-        backgroundIndex: 0,
-        backgroundMode: this.state.darkBackgroundModes[0]
-      });
-    } else if (this.checkIfDayModeEnabled()) {
-      this.setState({
-        appClass: 'nightlight',
-        backgroundIndex: 0,
-        backgroundMode: this.state.lightBackgroundModes[0]
-      });
-    }
-  };
+	render() {
+		const {
+			appClass,
+			bgStyle,
+			backgroundMode,
+			devIntro,
+			devDesc,
+			devDesc2,
+			icons,
+		} = this.state;
 
-  render() {
-    const {
-      appClass, bgStyle, backgroundMode, devIntro, devDesc, devDesc2, icons
-    } = this.state;
-
-    return (
-      <div className={ appClass } style={ bgStyle }>
-        <div className="change-mode" onClick={this.changeThemeMode} />
-        <div
-          className={ backgroundMode }
-        >
-          <main className="App-main">
-            <h1 className="intro">{ devIntro }</h1>
-            <div className="tagline">
-              <Typist>
-                <span>{ devDesc }</span>
-              <Typist.Backspace count={6} delay={450}/>
-              <span>{ devDesc2 }</span>
-              </Typist>
-            </div>
-            <div className="icons-social">
-              {icons.map(icon => (
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={ `${icon.url}` }
-                >
-                  <i className={ `fab ${icon.image}` } />
-                </a>
-              ))}
-            </div>
-            <br/>
-            <div className="footer">
-              <a  href="https://jespadas.github.io/covid-19/" target="_blank" rel="noopener noreferrer"><span class="coronavirus"> Project to follow COVID19 stats </span></a> <br/>
-              
-              <span>Made with <FaHeart /> and <FaBeer/> </span>
-            </div>
-          </main>
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className={appClass} style={bgStyle}>
+				<div className='change-mode' onClick={this.changeThemeMode} />
+				<div className={backgroundMode}>
+					<main className='App-main'>
+						<h1 className='intro'>{devIntro}</h1>
+            <img src={Me} alt='profile' style={{width: "8rem", borderRadius: "50%"}}/>
+						<div className='tagline'>
+							<ReactTyped
+								strings={[devDesc, devDesc2]}
+								typeSpeed={40}
+								backSpeed={40}
+								loop
+                smartBackspace
+							>
+							</ReactTyped>
+						</div>
+						<div className='icons-social'>
+							{icons.map((icon) => (
+								<a
+									target='_blank'
+									rel='noopener noreferrer'
+									href={`${icon.url}`}
+								>
+									<i className={`fab ${icon.image}`} />
+								</a>
+							))}
+						</div>
+						<br />
+						<div className='footer'>
+							<br />
+							<span>
+								Made with <FaHeart /> and <FaBeer />{' '}
+							</span>
+						</div>
+					</main>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
